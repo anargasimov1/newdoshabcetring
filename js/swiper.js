e532604f37ac8f83786309a4fc2da0b70ffc5711
anargasimov1 <anar.gasimov.89@gmail.com>
2024-07-13 23:12:41 +0400

    v1.0.0

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+// const swiper = document.getElementById("swiper")
+
+// console.log(swiper)
+
+
+
+// for (let i = 0; i <= 10; ++i) {
+//     swiper.innerHTML += `   <div class="item">
+//                                         <div class="product_blog_img">
+//                                             <img src="images/rs1.png" alt="#" />
+//                                         </div>
+//                                         <div class="product_blog_cont">
+//                                             <h3>Homemade</h3>
+//                                             <h4><span class="theme_color">10</span> AZN</h4>
+//                                         </div>
+//                                     </div>`
+// }
