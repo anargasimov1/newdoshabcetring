941aba4118ac73e79967aba04e33c829b4d2f647
anargasimov1 <anar.gasimov.89@gmail.com>
2024-07-14 16:15:01 +0400

    v1.1.0

--- js/swiper.js
+++ js/swiper.js
@@ -15,3 +15,25 @@
 //                                         </div>
 //                                     </div>`
 // }
+
+const name1 = document.getElementById("Name"),
+    phone = document.getElementById("Phone"),
+    message = document.getElementById("Message"),
+    email = document.getElementById("Email"),
+    btn = document.getElementById("btn");
+
+
+btn.onclick = () => {
+    let values = {
+        name: name1.value,
+        phone: phone.value,
+        email: email.value,
+        message: message.value
+    }
+
+    console.log(values)
+    name1.value = "",
+        phone.value = "",
+        message.value = "",
+        email.value = ""
+}
